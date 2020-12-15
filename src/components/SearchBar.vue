ad94eee921020da3d15a9a3f43904853688904f3
llabati <33926231+llabati@users.noreply.github.com>
2020-12-15 15:28:06 +0100

    Install a store for managing API call

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -5,6 +5,15 @@
 </template>
 
 <script>
+/* eslint-disable */
+    export function validateQuery(str){
+        const forbidden = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '@', '#', '&', '§', '(']
+        for (let letter of forbidden) {
+            if (str.startsWith(letter)) return false
+        }
+        return true
+    }
+
     export default {
         data(){
             return {
@@ -13,9 +22,13 @@
         },
         methods: {
             send(){
-                
                 console.log(this.query)
-                this.$emit('new-search', this.query)
+                if (validateQuery(this.query)) {
+                    this.$emit('new-search', this.query)
+                }
+                else {
+                    this.$emit('warning')
+                }
             }
         },
         directives: {
